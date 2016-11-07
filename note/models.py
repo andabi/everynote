4c5762b799062439b713c79521a7759c6b49f6df
dabi <andabi412@gmail.com>
2016-11-07 18:56:02 +0900

    note model 추가

--- note/models.py
+++ note/models.py
@@ -1,5 +1,10 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    lang1 = models.CharField(max_length=200)
+    lang2 = models.CharField(max_length=200)
+    date = models.DateTimeField('date')
+
+    def __str__(self):
+        return '%s, %s' % (self.lang1, self.lang2)
