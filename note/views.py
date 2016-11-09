63dccd4aef67941c858dde3e02e70c6f925d25f3
dabi <andabi412@gmail.com>
2016-11-09 17:38:45 +0900

    note list get draft

--- note/views.py
+++ note/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from django.core import serializers
 
+from .models import Note
 
+# TODO pagination
 def index(request):
-    return HttpResponse("")
+    max_count = 100
+    notes = Note.objects.order_by('date')[:max_count]
+    notes_as_json = serializers.serialize('json', notes, fields=('lang1','lang2','date'))
+    return HttpResponse(notes_as_json, content_type='json')
